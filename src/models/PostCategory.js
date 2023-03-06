1bc3bcd38c21e386ddc2710715c672ff35d5ec60
Cozmu <www.jorgereis@gmail.com>
2023-03-06 18:32:50 -0300

    feat: add model postCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const postCategory = sequelize.define('PostCategory',
+  {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  }, { underscored: true, timestamps: false, tableName: 'posts_categories' }); 
+  
+  postCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category,
+      {
+        foreignKey: 'categoryId',
+        otherKey: 'postId',
+        as: 'category',
+        through: postCategory,
+    });
+
+    models.Category.belongsToMany(models.BlogPost,
+      {
+        foreignKey: 'postId',
+        otherKey: 'categoryId',
+        as: 'blogPost',
+        through: postCategory,
+    });
+  }
+
+  return postCategory;
+};
